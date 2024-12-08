474b27b39762990a817c853f65b169e9b7097879
bak3839 <bak3839@naver.com>
2024-12-08 14:23:13 +0900

    nullability와 .? .!! 엘비스 연산자 학습

--- src/Person.kt
+++ src/Person.kt
@@ -8,24 +8,49 @@ class Rectangle(val height: Int, val width: Int) {
 }
 
 fun main() {
-    val persons = listOf(
-        Person("James", 32, true),
-        Person("Kameron", 42, true),
-        Person("Sphilberg", 52, true),
-        Person("Philip", 12, true),
+    val nullablePerson: Person? =
+        if (System.currentTimeMillis() % 2 == 0L) Person("Even", 22, true)
+        else null
+
+    val notNullPerson: Person = nullablePerson ?: Person(
+        "DefaultPerson", 0, false
     )
 
-    val result1:List<Char> = persons.asSequence()
-        .map { it.name }
-        .flatMap { it.toList() }
-        .toList()
+    println(notNullPerson.age)
 
-    val result2:String? = persons.asSequence()
-        .map { it.name }
-        .find { it.startsWith("J") }
+    // if 검사 등으로 특정 영역에서는 not null 타입처럼 사용 가능
+//    if (nullablePerson != null) {
+//        println("짝수 시간에만 존재하는 사람의 나이는 : ${nullablePerson.age}")
+//    }
+//
+//    println(nullablePerson?.age ?: 0)
+//
+//    // ?. 호출을 수행 : 값이 있는 경우는 호출 결과, 값이 없는 경우 null
+//    println("짝수 시간에만 존재하는 사람의 나이는 : ${nullablePerson?.age}")
+//
+//    // !!. 호출을 수행 : 값이 없는 경우 NPE가 발생
+//    println("짝수 시간에만 존재하는 사람의 나이는 : ${nullablePerson!!.age}")
 
-    println("result1 = " + result1)
-    println("result2 = " + result2)
+
+
+//    val persons = listOf(
+//        Person("James", 32, true),
+//        Person("Kameron", 42, true),
+//        Person("Sphilberg", 52, true),
+//        Person("Philip", 12, true),
+//    )
+//
+//    val result1:List<Char> = persons.asSequence()
+//        .map { it.name }
+//        .flatMap { it.toList() }
+//        .toList()
+//
+//    val result2:String? = persons.asSequence()
+//        .map { it.name }
+//        .find { it.startsWith("J") }
+//
+//    println("result1 = " + result1)
+//    println("result2 = " + result2)
 
 //    val result1:List<List<Char>> = persons.map {it.name}
 //        .map { it.toList() }
